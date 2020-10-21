7e0611377ff896afa6d45ad7deb0b8c784b5268c
Margaret Green <margaret@techelevator.com>
2020-10-21 16:04:33 -0400

    new setup script

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@
 echo
 read -r -p "Enter your name (First Last): " name
 read -r -p "Enter your email: " email
-read -r -p "Enter your cohort (c or java): " cohort
-read -r -p "Enter your bitbucket team (e.g. te-cin-cohort-4): " team
+# read -r -p "Enter your cohort (c or java): " cohort
+# read -r -p "Enter your bitbucket team (e.g. te-cin-cohort-4): " team
 
 echo
 echo "Setting Up Global Configuration Settings"
@@ -28,7 +28,9 @@ git config --global difftool.code.cmd "code -w -d \$LOCAL \$REMOTE"
 echo
 echo "Configuring Upstream..."
 
-git remote add upstream "https://bitbucket.org/${team}/${cohort}-main"
+git remote add upstream "https://bitbucket.org/te-nlr-2010/java-green-main"
 git config branch.master.mergeOptions "--no-edit"
 
 echo "Done."
+
+# just testing
