212131bea20d5603b72199966073c5ca988028c5
Margaret Green <mlanegreen@gmail.com>
2020-10-29 12:04:02 -0400

    adding day 4 exercise folder

--- module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -169,6 +169,18 @@ public class Exercises {
 	 max1020(11, 9) → 11
 	 */
 	public int max1020(int a, int b) {
+		if (a > 9 && a < 21 && b > 9 && b < 21 && a >= b) {
+			return a;
+		}
+		else if (a > 9 && a < 21 && b > 9 && b < 21 && b >= a) {
+			return b;
+		}
+		else if (a > 9 && a < 21 && !(b > 9 && b < 21)) {
+			 return a;
+		}
+		else if (b > 9 && b < 21 && !(a > 9 && a < 21)) {
+			return b;
+		}
 		return 0;
 	}
 
@@ -418,6 +430,20 @@ public class Exercises {
 	 luckySum(13, 13, 3) → 0
 	 */
 	public int luckySum(int a, int b, int c) {
+		int sum = a + b + c; //this is the sum if no number is 13
+		
+		if (a != 13 && b != 13 && c != 13) {
+			return (a + b + c);
+		}
+		else if (a == 13 && b != 13 && c != 13) {
+			return c;
+		}
+		else if (a != 13 && b == 13 && c != 13) {
+			return a;
+		}
+		else if (a != 13 && b != 13 && c == 13) {
+			return a + b;
+		}
 		return 0;
 	}
 
