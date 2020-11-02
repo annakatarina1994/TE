663fd53ed11c515aa8d4fb1449c37cb7e6d0e7dd
Anna Lizon <annakatarina1994@gmail.com>
2020-11-02 14:36:06 -0500

    Completed decimal to binary activity

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,20 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		System.out.println("Please enter a series of decimal numbers separated by spaces: ");
+		Scanner input = new Scanner(System.in);
+		String decimalNumsInput = input.nextLine();
+		String[] decimalNums = decimalNumsInput.split(" ");
 		
+		for(int i = 0; i < decimalNums.length; i++) {
+			int decimalNum = Integer.parseInt(decimalNums[i]);
+			String binaryNum = Integer.toBinaryString(decimalNum);
+			System.out.println(decimalNum + " in binary is " + binaryNum);
+		}
 	}
 
 }
