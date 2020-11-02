cefcb31b6d2cea3bb65663e8361139dcb3a722ae
Anna Lizon <annakatarina1994@gmail.com>
2020-11-02 14:18:59 -0500

    Completed fibonacci activity

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,28 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
-
+	
 	public static void main(String[] args) {
-
+			System.out.println("Please enter the ending Fibonacci number: ");
+			Scanner input = new Scanner(System.in);
+			int endingNumber = input.nextInt();
+			
+			int a = 0;
+			int b = 1;
+			
+			System.out.println(a);
+			
+			int sum = 0;
+			
+			while(sum <= endingNumber) {
+				System.out.println(b);
+				sum = a + b;
+				a = b;
+				b = sum;
+			}
+			
 	}
 
 }
