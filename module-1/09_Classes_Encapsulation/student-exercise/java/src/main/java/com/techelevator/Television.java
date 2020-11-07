2741fde3f3c5479f9d603bb1c8371ca7d8880d7f
Anna Lizon <annakatarina1994@gmail.com>
2020-11-07 15:42:08 -0500

    Completed fruit tree and employee exercises

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,64 @@
 package com.techelevator;
 
 public class Television {
+	
+	private boolean isOn = false;
+	private int currentChannel = 3;
+	private int currentVolume = 2;
+	
+	public boolean isOn() {
+		return isOn;
+	}
+	
+	public int getCurrentChannel() {
+		return currentChannel;
+	}
+	
+	public int getCurrentVolume() {
+		return currentVolume;
+	}
+	
+	public void turnOff() {
+		this.isOn = !isOn;
+	}
+	
+	public void turnOn() {
+		this.isOn = true;
+		this.currentChannel = 3;
+		this.currentVolume = 2;
+	}
+	
+	public void changeChannel(int newChannel) {
+		if(this.isOn = true && newChannel >= 3 && newChannel <= 18) {
+			currentChannel = newChannel;
+		} 
+	}
+	
+	public void channelUp() {
+		if(this.isOn = true && currentChannel <= 18) {
+			currentChannel += 1;
+		}else if(this.isOn = true && currentChannel > 18) {
+			currentChannel = 3;
+		} 
+	} 
+	
+	public void channelDown() {
+		if(this.isOn = true && currentChannel >= 3) {
+			currentChannel -= 1;
+		}else if(this.isOn = true && currentChannel < 3) {
+			currentChannel = 18;
+		}
+	}
 
+	public void raiseVolume() {
+		if(isOn = true && currentVolume <= 10) {
+			currentVolume += 1;
+		}
+	}
+	
+	public void lowerVolume() {
+		if(this.isOn = true && currentVolume >= 0) {
+			currentVolume -= 1;
+		}
+	}
 }
--- module-1/09_Classes_Encapsulation/student-tutorial/java/src/main/java/com/techelevator/Bookstore.java
+++ module-1/09_Classes_Encapsulation/student-tutorial/java/src/main/java/com/techelevator/Bookstore.java
@@ -11,9 +11,26 @@ public class Bookstore {
         System.out.println();
 
         // Step Three: Test the getters and setters
+        
+        Book twoCities = new Book();
+        twoCities.setTitle("A Tale Of Two Cities");
+        twoCities.setAuthor("Charles Dickens");
+        twoCities.setPrice(14.99);
+        
+//        System.out.println("Title: " + twoCities.getTitle() + ", Author: " + twoCities.getAuthor() + ", Price: $" + twoCities.getPrice());
 
         // Step Five: Test the Book constructor
+        
+        Book threeMusketeers = new Book("The Three Musketeers", "Alexandre Dumas", 12.95);
+        Book childhoodEnd = new Book("Childhood's End", "Arhtur C. Clark", 5.99);
+        
+//        System.out.println("Title: " + threeMusketeers.getTitle() + ", Author: " + threeMusketeers.getAuthor() + ", Price: $" + threeMusketeers.getPrice());
+//        System.out.println("Title: " + childhoodEnd.getTitle() + ", Author: " + childhoodEnd.getAuthor() + ", Price: $" + childhoodEnd.getPrice());
 
+        System.out.println(twoCities.bookInfo());
+        System.out.println(threeMusketeers.bookInfo());
+        System.out.println(childhoodEnd.bookInfo());
+        
         // Step Nine: Test the ShoppingCart class
         
     }
