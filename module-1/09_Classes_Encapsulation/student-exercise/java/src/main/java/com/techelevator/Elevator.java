94e69b9493d9e9984b31c4be3c6c6dfd0292c5ac
Anna Lizon <annakatarina1994@gmail.com>
2020-11-08 18:59:55 -0500

    Completed elevator exercise

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,47 @@
 package com.techelevator;
 
 public class Elevator {
+	
+	private int currentFloor;
+	private int numberOfFloors;
+	private boolean doorOpen;
+	
+	
 
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+
+	public boolean isDoorOpen() {
+		return doorOpen;
+	}
+
+	public Elevator(int numberOfFloors) {
+		this.currentFloor = 1;
+		this.numberOfFloors = numberOfFloors;
+	}
+	
+	public void openDoor() {
+		this.doorOpen = true;
+	}
+	
+	public void closeDoor() {
+		this.doorOpen = false;
+	}
+	
+	public void goUp(int desiredFloor) {
+		if(this.doorOpen == false && desiredFloor <= numberOfFloors && desiredFloor > currentFloor) {
+			currentFloor = desiredFloor;
+		}
+	}
+	
+	public void goDown(int desiredFloor) {
+		if(this.doorOpen == false && desiredFloor >= 1 && desiredFloor < currentFloor) {
+			currentFloor = desiredFloor;
+		}
+	}
 }
