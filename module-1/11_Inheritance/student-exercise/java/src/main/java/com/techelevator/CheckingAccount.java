6a612810cd2d35f644273193bb57ee549f0d965e
Anna Lizon <annakatarina1994@gmail.com>
2020-11-09 19:19:35 -0500

    Completed Checking Account override method

---
+++ module-1/11_Inheritance/student-exercise/java/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,29 @@
+package com.techelevator;
+
+public class CheckingAccount extends BankAccount {
+
+	public CheckingAccount(String accountHolderName, String accountNumber) {
+		super(accountHolderName, accountNumber);
+		
+	}
+	
+	public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+		super(accountHolderName, accountNumber, balance);
+		
+	}
+	
+	@Override
+	public int withdraw(int amountToWithdraw) {
+		if((getBalance() - amountToWithdraw < 0) && (getBalance() - amountToWithdraw > -100)) {
+			super.withdraw(amountToWithdraw + 10);
+			return getBalance();
+		} else if(getBalance() - amountToWithdraw < -100) {
+			return getBalance();
+		} else if(getBalance() - amountToWithdraw > 0) {
+			super.withdraw(amountToWithdraw);
+			return getBalance();
+		}
+		return 0;
+	}
+	
+}
---
+++ module-1/11_Inheritance/student-exercise/java/src/main/java/com/techelevator/SavingsAccount.java
@@ -0,0 +1,10 @@
+package com.techelevator;
+
+public class SavingsAccount extends BankAccount{
+
+	public SavingsAccount(String accountHolderName, String accountNumber) {
+		this.accountHolderName = accountHolderName;
+		this.accountNumber = accountNumber;
+	}
+	
+}
