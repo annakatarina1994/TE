9d3ba840974bc463d1c48f95cd6a97a96504beeb
Anna Lizon <annakatarina1994@gmail.com>
2020-11-09 19:31:39 -0500

    Completed Savings Account override method

--- module-1/11_Inheritance/student-exercise/java/src/main/java/com/techelevator/SavingsAccount.java
+++ module-1/11_Inheritance/student-exercise/java/src/main/java/com/techelevator/SavingsAccount.java
@@ -3,8 +3,27 @@ package com.techelevator;
 public class SavingsAccount extends BankAccount{
 
 	public SavingsAccount(String accountHolderName, String accountNumber) {
-		this.accountHolderName = accountHolderName;
-		this.accountNumber = accountNumber;
+		super(accountHolderName, accountNumber);
 	}
+	
+	public SavingsAccount(String accountHolderName, String accountNumber, int balance) {
+		super(accountHolderName, accountNumber, balance);
+	}
+	
+	@Override
+	public int withdraw(int amountToWithdraw) {
+		if(getBalance() - amountToWithdraw < 0) {
+			return getBalance();
+		}else if(getBalance() - amountToWithdraw < 150) {
+			super.withdraw(amountToWithdraw + 2);
+			return getBalance();
+		}else if(amountToWithdraw > getBalance()) {
+			return getBalance();
+		} else if(getBalance() - amountToWithdraw >= 150) {
+			super.withdraw(amountToWithdraw);
+			return getBalance();
+		
+		}return 0;
+		} 
 	
 }
