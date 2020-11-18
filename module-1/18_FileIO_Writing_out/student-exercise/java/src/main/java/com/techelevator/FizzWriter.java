66cd20a044ea83b48a7ce5afeba6cbca0bf2dc56
Anna Lizon <annakatarina1994@gmail.com>
2020-11-17 20:42:25 -0500

    New file isn't being created...not sure what I'm missing...

--- module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,34 @@
 package com.techelevator;
 
-public class FizzWriter {
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
+public class FizzWriter{
 
+	public static void main(String[] args) throws IOException {
+		
+		
+		String filePath = "FizzBuzz.txt";
+		File fizzBuzzFile = new File(filePath);
+		fizzBuzzFile.createNewFile();
+	
+		try(PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))){
+		
+		for(int i = 0; i <= 300; i++) {
+			if(i % 15 == 0) {
+				writer.write("FizzBuzz");
+			} else if(i % 5 == 0) {
+				writer.write("Buzz");
+			} else if(i % 3 == 0) {
+				writer.write("Fizz");
+			} else {
+				writer.write(i);
+				}
+			}
+		}
 	}
-
 }
+
