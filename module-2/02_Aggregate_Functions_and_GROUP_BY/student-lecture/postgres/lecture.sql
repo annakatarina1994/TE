6f98cea58916f63f1ee6d36b3f1674b9f106adfd
Anna Lizon <annakatarina1994@gmail.com>
2020-11-24 14:56:04 -0500

    Completed aggregate functions exercise

--- module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/postgres/lecture.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/student-lecture/postgres/lecture.sql
@@ -1,43 +1,87 @@
 -- ORDERING RESULTS
 
 -- Populations of all countries in descending order
+SELECT name, population FROM country ORDER BY population DESC;
 
 --Names of countries and continents in ascending order
+SELECT name, continent FROM country ORDER BY continent ASC, name ASC;
 
 -- LIMITING RESULTS
 -- The name and average life expectancy of the countries with the 10 highest life expectancies.
+SELECT name, lifeexpectancy 
+FROM country 
+WHERE lifeexpectancy IS NOT NULL 
+ORDER BY lifeexpectancy   
+DESC LIMIT 10;
 
 -- CONCATENATING OUTPUTS
 
 -- The name & state of all cities in California, Oregon, or Washington.
 -- "city, state", sorted by state then city
+SELECT CONCAT (city.name, ', ', district) AS citystatename
+FROM city
+WHERE district IN ('California', 'Oregon', 'Washington');
 
 -- AGGREGATE FUNCTIONS
 -- Average Life Expectancy in the World
+SELECT AVG(lifeexpectancy) AS avgLifeExpectancy FROM country;
 
 -- Total population in Ohio
+SELECT SUM(population) AS ohioPopulation FROM city WHERE district = 'Ohio';
 
 -- The surface area of the smallest country in the world
+SELECT MIN(surfacearea || ' ' || name) FROM country;
 
 -- The 10 largest countries in the world
+SELECT name, surfacearea FROM country ORDER BY surfacearea DESC LIMIT 10;
 
 -- The number of countries who declared independence in 1991
+SELECT COUNT(indepyear) FROM country WHERE indepyear = '1991';
 
 -- GROUP BY
 -- Count the number of countries where each language is spoken, ordered from most countries to least
+SELECT language, COUNT(countrycode) AS countries FROM countrylanguage
+GROUP BY language
+ORDER BY countries DESC;
 
 -- Average life expectancy of each continent ordered from highest to lowest
+SELECT continent, AVG(lifeexpectancy) AS avgLifeExpectancy FROM country
+GROUP BY continent ORDER BY avgLifeExpectancy DESC;
 
 -- Exclude Antarctica from consideration for average life expectancy
+SELECT continent, AVG(lifeexpectancy) AS avgLifeExpectancy FROM country
+WHERE lifeexpectancy IS NOT NULL
+GROUP BY continent ORDER BY avgLifeExpectancy DESC;
 
 -- Sum of the population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population)
+ FROM city
+ WHERE countrycode = 'USA' 
+ GROUP BY district
+ ORDER BY district;
 
 -- The average population of cities in each state in the USA ordered by state name
+SELECT district, AVG(population)
+ FROM city
+ WHERE countrycode = 'USA' 
+ GROUP BY district
+ ORDER BY district;
 
 -- SUBQUERIES
 -- Find the names of cities under a given government leader
+SELECT name, code FROM country 
+WHERE headofstate = 'Elisabeth II';
+
+SELECT name, district FROM city WHERE countrycode IN 
+(SELECT code FROM country 
+WHERE headofstate = 'Elisabeth II');   --subquery
 
 -- Find the names of cities whose country they belong to has not declared independence yet
+
+SELECT city.name, city.district, city.countrycode, country.headofstate
+FROM city, country
+WHERE country.code IN 
+(SELECT code FROM country WHERE indepyear IS NULL);
 
 -- Additional samples
 -- You may alias column and table names to be more descriptive
