647238a15139e13104ded8152c681ac84e6c6267
Anna Lizon <annakatarina1994@gmail.com>
2020-11-29 20:25:14 -0500

    Completed exercises 7-12

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -74,26 +74,52 @@
         WHERE film_id = 996;
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
-
+    INSERT INTO inventory(inventory_id, film_id, store_id)
+    VALUES (5000, 1001, 1);
+    
+     INSERT INTO inventory(inventory_id, film_id, store_id)
+    VALUES (5001, 1001, 2);
+    
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- < It did not succeed and it looks like it's due to the fact that other tables are already connected to
+--        Euclidean PI (due to a foreign key constraint). For instance, the film_actor table still references the movie
+--        so it can't just be removed at this point  >
+
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <  It doesn't work for a similar reason as above, except this time, the category can't be deleted becase
+--    it still has a reference in the film_category table  >
+
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <  This time it worked and I *think* it's because we're deleting the primary key...   >
+        DELETE FROM film_category
+        WHERE category_id = 17;
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <  Deleting Mathmagical from the category table worked and I imagine it's because we deleted the 
+--      primary key for the Mathmagical category. However, deleting Euclidean PI from the film table
+--      still doesn't work. I think it's because film_actor still relies on a foreign key from that movie  >
+        DELETE FROM category
+        WHERE name = 'Mathmagical';
+        
+        DELETE FROM film
+        WHERE title = 'Euclidean PI';
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+    -- <  I think that if we just deleted the actor_id and film_id that's connecting the actors we added into Euclidean PI in the beginning, 
+   --         we would be able to delete Euclidean PI from the film table. From what I tried below, I believe that worked.   >
+   
+        BEGIN TRANSACTION;
+        DELETE FROM film_actor WHERE film_id = 1001;
+        DELETE FROM film WHERE title = 'Eucliean PI';
+        ROLLBACK;
