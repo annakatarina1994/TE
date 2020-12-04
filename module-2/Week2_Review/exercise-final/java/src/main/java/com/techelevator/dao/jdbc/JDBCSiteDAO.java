d8fb54d28cb79eb38760832477626af3dfa43ae1
margaretgreen <margaret@techelevator.com>
2020-12-04 12:24:23 -0500

    added lecture final for week2 review

---
+++ module-2/Week2_Review/exercise-final/java/src/main/java/com/techelevator/dao/jdbc/JDBCSiteDAO.java
@@ -0,0 +1,45 @@
+package com.techelevator.dao.jdbc;
+
+import com.techelevator.dao.SiteDAO;
+import com.techelevator.model.Site;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCSiteDAO implements SiteDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JDBCSiteDAO(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<Site> getSitesThatAllowRVs(int parkId) {
+    	List<Site> site = new ArrayList<>();
+    	String sqlGetSitesRv = " SELECT * FROM site JOIN campground ON site.campground_id = campground.campground_id WHERE site.max_rv_length > 0 AND campground.park_id = ?";
+    	
+    	SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetSitesRv, parkId);
+    	while (results.next()) {
+    		Site sites = mapRowToSite(results);
+    		site.add(sites);
+    	}
+        return site;
+    }
+
+    private Site mapRowToSite(SqlRowSet results) {
+        Site site = new Site();
+        site.setSiteId(results.getInt("site_id"));
+        site.setCampgroundId(results.getInt("campground_id"));
+        site.setSiteNumber(results.getInt("site_number"));
+        site.setMaxOccupancy(results.getInt("max_occupancy"));
+        site.setAccessible(results.getBoolean("accessible"));
+        site.setMaxRvLength(results.getInt("max_rv_length"));
+        site.setUtilities(results.getBoolean("utilities"));
+        return site;
+    }
+}
---
+++ module-2/Week2_Review/exercise-final/java/src/test/java/com/techelevator/dao/jdbc/JDBCParkDAOTests.java
@@ -0,0 +1,56 @@
+package com.techelevator.dao.jdbc;
+
+import com.techelevator.dao.ParkDAO;
+import com.techelevator.model.Park;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class JDBCParkDAOTests extends BaseDAOTests {
+
+    private ParkDAO dao;
+
+    @Before
+    public void setup() {
+        dao = new JDBCParkDAO(dataSource);
+
+    }
+
+//    @Test
+//    public void getParksTest_Should_ReturnAllParksInLocationAlphaOrder() {
+//        List<Park> parks = dao.getAllParks();
+//
+//        assertEquals(3, parks.size());
+//        assertEquals("Maine", parks.get(0).getLocation());
+//        assertEquals("Ohio", parks.get(1).getLocation());
+//        assertEquals("Utah", parks.get(2).getLocation());
+//    }
+    
+    @Test
+    public void getParksTest_should_return_one_more_after_createPark() {
+    	List<Park> parks = dao.getAllParks();
+    	int sizeBeforeAdd = parks.size();
+    	
+        Park park = new Park();
+        park.setName("SQL PARK");
+        park.setLocation("SQLCITY");
+        park.setEstablishDate(LocalDate.now());
+        park.setArea(2000);
+        park.setVisitors(2000);
+        park.setDescription("SQL IS GREAT FUN");
+        
+        dao.createNewPark(park);
+        
+        List<Park> parksAfterAdd = dao.getAllParks();
+    	int sizeAfterAdd = parksAfterAdd.size();
+    	
+    	assertEquals(sizeBeforeAdd + 1, sizeAfterAdd);
+    
+    }
+
+}
