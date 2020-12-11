a29fb6b9bba28d58b35c9ed1f34787b80afda03f
margaretgreen <margaret@techelevator.com>
2020-12-11 07:23:08 -0500

    pushing today's lecture code -- Cat cards!

---
+++ module-2/Week3_Review/java/student/src/main/java/com/techelevator/services/RestCatFactService.java
@@ -0,0 +1,16 @@
+package com.techelevator.services;
+
+import org.springframework.stereotype.Component;
+
+import com.techelevator.model.CatFact;
+
+
+public class RestCatFactService implements CatFactService {
+
+	@Override
+	public CatFact getFact() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
