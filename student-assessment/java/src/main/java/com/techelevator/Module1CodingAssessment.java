e9ee760902201d19d952e23a7315a3f1c8fabdee
Anna Lizon <annakatarina1994@gmail.com>
2020-12-31 13:30:32 -0500

    Reading in file, just have to figure out how to add up the ages..

--- student-assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ student-assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -1,13 +1,44 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Module1CodingAssessment {
 
-	public static void main(String[] args) {
-		Car viper = new Car(2000, "Dodge", false);
-		Car oldCorvette = new Car(1957, "Chevy", true);
+	public static void main(String[] args) throws FileNotFoundException {
+		File carFile = new File("data-files/CarInput.csv");
+		List cars = new ArrayList<>();
 		
-		System.out.println(viper.carNeedsECheck(2020));
-		System.out.println(viper.toString());
+		try(Scanner fileScanner = new Scanner(carFile)){
+			while(fileScanner.hasNextLine()) {
+				String line = fileScanner.nextLine();
+				String[] carArray = line.split(",");
+				
+				int year = Integer.parseInt(carArray[0]);
+				String make = carArray[1];
+				boolean isClassicCar = Boolean.parseBoolean(carArray[2]);
+				
+				Car car = new Car(year, make, isClassicCar);
+				cars.add(car);
+				
+				int age = car.getAge(2020);
+				System.out.println(age);
+			}
+			System.out.println(cars.toString());
+			
+		}
+		
+		
+		
+		
+//		Car viper = new Car(2000, "Dodge", false);
+//		Car oldCorvette = new Car(1957, "Chevy", true);
+//		
+//		System.out.println(viper.carNeedsECheck(2020));
+//		System.out.println(viper.toString());
 	}
 
 	
