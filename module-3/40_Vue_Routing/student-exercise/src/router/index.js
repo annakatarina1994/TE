1398975cd937568d4ad72810f7388a647c916a4a
Anna Lizon <annakatarina1994@gmail.com>
2021-01-26 16:54:36 -0500

    tests passing!

--- module-3/40_Vue_Routing/student-exercise/src/router/index.js
+++ module-3/40_Vue_Routing/student-exercise/src/router/index.js
@@ -1,10 +1,33 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/views/Home';
+import MyBooks from '@/views/MyBooks';
+import AddBook from '@/views/AddBook';
+import BookDetails from '@/views/BookDetails';
 
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/myBooks',
+    name: 'myBooks',
+    component: MyBooks
+  },
+  {
+    path: '/addBook',
+    name: 'addBook',
+    component: AddBook
+  },
+  {
+    path: '/book/:isbn',
+    name: 'bookDetails',
+    component: BookDetails
+  }
 ];
 
 const router = new VueRouter({
---
+++ module-3/40_Vue_Routing/student-exercise/src/views/BookDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <div>
+    <h2 class="book-title">{{ book.title }}</h2>
+    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
+    <h3 class="book-author">{{ book.author }}</h3>
+    <p v-if="!book.read">You have NOT read this book.</p>
+    <p v-if="book.read">You have read this book!</p>
+  </div> -->
+  <div>
+      <book-card v-for="book in $store.state.books" v-bind:book="book" v-bind:key="book.isbn"/>
+  </div>
+
+</template>
+
+<script>
+import BookCard from '../components/BookCard.vue';
+
+export default {
+  components: { BookCard },
+  props: ["book"]
+};
+</script>
+
+<style>
+</style>
