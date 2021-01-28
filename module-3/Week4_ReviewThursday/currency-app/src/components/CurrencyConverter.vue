c2281a8e82915207c2462222ab4cea461f33baae
Peggy Green <mlanegreen@gmail.com>
2021-01-28 12:38:24 -0500

    added currency-app from Thursday review

---
+++ module-3/Week4_ReviewThursday/currency-app/src/components/CurrencyConverter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="currency-converter">
+      <h1>Currency Converter</h1>
+      <h2>Convert from USD to {{ $store.state.rateObj.name }}</h2>
+      <form class="convertForm">
+          <label for="amount">Enter amount to convert: </label>
+          <input type="number" id="amount" name="amount" v-model="amountToConvert" />
+      </form>
+      <h2 v-if="amountToConvert != '' && $store.state.rateObj != ''">${{ convertedAmount }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            amountToConvert: ''
+        }
+    },
+    computed: {
+        convertedAmount() {
+            return ( (this.amountToConvert * this.$store.state.rateObj.rate).toFixed(2));
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
